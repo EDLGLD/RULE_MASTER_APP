cc960b79647f60a936e05769c3b5fcb53b2bd588
edlgld <muramasa3k@gmail.com>
2024-10-01 23:34:32 +0900

    Rulesのシードデータを追加、User⇔team_nameを多対多に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :team_name, optional: true  # チームなしでもユーザーを作成可能
+  has_and_belongs_to_many :team_names, optional: true  # チームなしでもユーザーを作成可能
   
   enum role: { general: 0, admin: 1, leader: 2 }
   validates :username, length: { maximum: 20 }, presence: true, uniqueness: true
